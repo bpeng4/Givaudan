7f99057f635ea7d244e11956b3bdb01166f29667
bpeng4 <bpeng4@huskers.unl.edu>
2025-07-09 11:46:02 -0500

    SCFAs_Update

---
+++ Profiling/SCFAs_Plotting.R
@@ -0,0 +1,117 @@
+library(ggplot2)
+library(ggpubr)
+library(dplyr)
+library(stringi)
+library(stringr)
+
+
+set1<-read.csv(file = "/Users/bopeng/Documents/Givaudan/Cache/FinalPlate1-12New.csv")
+set2<-read.csv(file = "/Users/bopeng/Documents/Givaudan/Cache/Final_Givaudan_AiMS_Meta_Map-3.csv")
+set3<- merge(set1, set2, by = "Sample_Num")
+
+Givaudan_SCFA<- reshape(data = set3,
+                      varying = c( "Acetate","Propionate","Isobutyrate", 
+                                  "Butyrate","Isovalerate", "Valeric.Acid","sum.ace.pro.buty.", "sum.isobuty.isoval."), 
+                      v.names = "SCFAs",
+                      direction = "long")
+Givaudan_SCFA$Type<-c(rep(x="Acetate",810),rep(x="Propionate",810),rep(x="Isobutyrate",810),rep(x="Butyrate",810),rep(x="Isovalerate",810),
+                  rep(x="Valeric.Acid",810),rep(x="Total_SCFAs.",810),rep(x="Total_BCFAs.",810))
+
+#Order the SCFAs levels
+# Set desired order
+desired_order <- c("Acetate", "Butyrate", "Propionate", "Valeric.Acid", "Isobutyrate", "Isovalerate",  "Total_SCFAs.",
+                   "Total_BCFAs.")
+
+# Make sure your SCFA type column is a factor with the desired order
+Givaudan_SCFA$Type <- factor(Givaudan_SCFA$Type, levels = desired_order)
+
+#Remove Spaces within sample names
+Givaudan_SCFA$Sample_Name <- stri_trim_both(Givaudan_SCFA$Sample_Name)
+Givaudan_SCFA$Sample_Name <- str_trim(str_replace_all(Givaudan_SCFA$Sample_Name, "\\p{Zs}+", ""))
+
+#Order Samples by the sample names
+sample_order <- c("Super B-glucan (SBG)","Oat B-glucans (OBG 70% (Low m.wt))- Garuda",
+                  "lantamanen OBG-29% GF","OBG 28% (OatWell Bran)",
+                  "Yeast B-glucans (YBG-Wellmune)","Gingest",
+                  "Inulin","AXOS", "Agrifiber", "Acerola full spectrum", 
+                  "Red Acerola 20% Vit C", "Green Acerola 34% Vit C", 
+                  "Acerola red 20% vit C& acerola green vit C 34%", "Ascorbic acid (Vit c)", 
+                  "Carrot juice + Green Acerola","Carrot juice pro vit A", 
+                  "Acerola green + OBG 28%", "Resistant starch postbiotic candidate 1",
+                  "Resistant starch postbiotic candidate 2")
+
+# Set factor levels
+Givaudan_SCFA$Sample <- factor(Givaudan_SCFA$Sample, levels = sample_order)
+
+# Arrange data frame by the desired order
+Givaudan_SCFA <- Givaudan_SCFA %>% arrange(Sample_Abbr)
+
+#Remove bad data
+Givaudan_SCFA_clean <- Givaudan_SCFA %>%
+  filter(!is.na(SCFAs), !is.nan(SCFAs), !is.infinite(SCFAs))
+
+anova_model <- aov(SCFAs ~ Type * Microbiome * Sample *Rep, data = Givaudan_SCFA_clean)
+summary(anova_model)
+
+#Plot and with anova test for SCFAs comparison among Samples
+# Step 1: Calculate 95th percentile for each Type
+quantile_limits <- Givaudan_SCFA_clean %>%
+  group_by(Type) %>%
+  summarise(q95 = quantile(SCFAs, 0.95, na.rm = TRUE))
+
+# Step 2: Join back to original data and filter
+Givaudan_SCFA_trimmed <- Givaudan_SCFA_clean %>%
+  left_join(quantile_limits, by = "Type") %>%
+  filter(SCFAs <= q95)
+
+# Step 3: Plot with trimmed data
+ggplot(Givaudan_SCFA_trimmed, aes(x = Sample, y = SCFAs, fill = Sample)) +
+  geom_boxplot() +
+  facet_wrap(~Type, scales = "free_y") +
+  stat_compare_means(method = "anova", size = 3, label.x = 2.5) +
+  labs(
+    x = "SCFAs Type", 
+    y = "Concentration (mM)", 
+    title = "SCFAs Concentration by Sample Type"
+  ) +
+  theme(
+    axis.title = element_text(size = 16),
+    axis.text.x = element_text(size = 5, angle = 90),
+    axis.text.y = element_text(size = 13),
+    legend.title = element_text(size = 18),
+    legend.text = element_text(size = 16),
+    plot.title = element_text(size = 17)
+  )
+
+#for each subset of microbiomes
+ # Define plotting function
+ Sample_SCFAs <- function(Givaudan_SCFA, Microbiome) {
+   # Create plot
+   ggplot(Givaudan_SCFA_trimmed, aes(x = Sample, y = SCFAs, fill = Sample)) +
+     geom_boxplot() +
+     facet_wrap(~Type, scales = "free_y") +
+     stat_compare_means(method = "anova", size = 3, label.x = 2.5) +
+     labs(
+       x = "SCFAs Type", 
+       y = "Concentration (mM)", 
+       title = paste0(Microbiome, " SCFAs Concentration by Sample Type")
+     ) +
+     theme(
+       axis.title = element_text(size = 16),
+       axis.text.x = element_text(size = 5, angle = 90),
+       axis.text.y = element_text(size = 13),
+       legend.title = element_text(size = 18),
+       legend.text = element_text(size = 16),
+       plot.title = element_text(size = 17)
+     )
+ }
+ 
+ # Ensure `Givaudan_SCFA` is defined if using it for unique Microbiome extraction
+ Microbiome_list <- unique(Givaudan_SCFA$Microbiome)
+ 
+ # Loop over each microbiome group
+ for (i in Microbiome_list) {
+   Sub_Givaudan_SCFA <- Givaudan_SCFA[Givaudan_SCFA$Microbiome == i, ]
+   print(Sample_SCFAs(Sub_Givaudan_SCFA, i))
+ }
+ 
